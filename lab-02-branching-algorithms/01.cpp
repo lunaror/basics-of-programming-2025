b3547cb1218e52681ce2bbf19fa7d905fb3c20ef
lunaror <121626993+lunaror@users.noreply.github.com>
2025-12-09 19:26:56 +0400

    add solution to task 1 of lab 2

---
+++ lab-02-branching-algorithms/01.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a = -0.5;
+    float b = 2;
+
+    float t, y;
+    cin >> t;
+
+    if (t < 2) {
+        y = a * pow(t, 2) * log(t);
+    } else if (t == 2) {
+        y = 1;
+    } else { // i.e. t > 2
+        y = a * cos(b * t);
+    }
+
+    cout << "y = " << y << '\n';
+
+    return 0;
+}
