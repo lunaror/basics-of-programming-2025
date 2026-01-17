e66e6d34e6e2ce6ccaaf81c25240af8df0293928
lunaror <121626993+lunaror@users.noreply.github.com>
2026-01-17 15:16:33 +0400

    add solution 10 for lab 6 (string)

---
+++ lab-06-string-processing/10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string given = "aAbBcCdDeE01020304";
+    string odd_pos_string = "";
+
+    for (int i = 0; i < given.length(); i += 2) {
+        // Append one letter with index i from to the end of the string.
+        odd_pos_string.insert(odd_pos_string.length(), 1, given[i]);
+    }
+
+    cout << odd_pos_string << '\n';
+
+    return 0;
+}
