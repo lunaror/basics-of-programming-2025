8d0b27a5363984be0682a3cbd612773004f9696c
lunaror <121626993+lunaror@users.noreply.github.com>
2026-01-17 15:05:20 +0400

    add note for solution 9

--- lab-06-string-processing/09.cpp
+++ lab-06-string-processing/09.cpp
@@ -6,6 +6,8 @@ int main() {
     string given = "aAbBcCdDeE01020304";
     string even_pos_string = "";
 
+    // Note that in this case we start from 1, not from 0, as 1 the first even
+    // position (2nd position).
     for (int i = 1; i < given.length(); i += 2) {
         // Append one letter with index i from to the end of the string.
         even_pos_string.insert(even_pos_string.length(), 1, given[i]);
